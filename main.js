5c679dbb17eea1a28a59e91b5e32dc8a66e6d708
Mustafa Hussein <mustafahussein.a@gmail.com>
2023-07-25 22:33:19 -0400

    add event listener

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('connected')
+console.log('main.js connected')
+
+const handleFormInputFocus = () => {
+    console.log(`focus occured`)
+}
+
+const searchTermsInput = document.body.querySelector("#search-terms")
+searchTermsInput.addEventListener('focus', handleFormInputFocus)
