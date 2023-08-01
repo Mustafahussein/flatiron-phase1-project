8cc95b00c93095de6395888834a4c74633f47e8c
Mustafa Hussein <mustafahussein.a@gmail.com>
2023-08-01 01:34:12 -0400

    making searchbox function

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // API Endpoint
+    // API endpoint
     const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
   
     // Event listeners
     const pokemonList = document.getElementById('pokemon-list');
+    const searchBox = document.getElementById('search-box');
     const pokemonInfo = document.getElementById('pokemon-info');
   
     // Fetch Pokémon data from the API
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        // Display the list of Pokémon names with images
+        // Store the Pokémon data for filtering
         const pokemonData = data.results;
-        pokemonData.forEach(pokemon => {
-          const listItem = document.createElement('li');
-          listItem.textContent = pokemon.name;
   
-          // Fetch the Pokémon details to get the sprites
-          fetch(pokemon.url)
-            .then(response => response.json())
-            .then(pokemonDetails => {
-              const spriteURL = pokemonDetails.sprites.front_default;
-              const image = document.createElement('img');
-              image.src = spriteURL;
-              listItem.prepend(image);
-            })
-            .catch(error => console.error('Error fetching Pokémon details:', error));
+        // Function to display the Pokémon list based on the search query
+        function displayPokemonList(query = '') {
+          pokemonList.innerHTML = '';
+          pokemonData.forEach(pokemon => {
+            if (pokemon.name.includes(query.toLowerCase())) {
+              const listItem = document.createElement('li');
+              listItem.textContent = pokemon.name;
   
-          listItem.addEventListener('click', () => fetchPokemonInfo(pokemon.url));
-          pokemonList.appendChild(listItem);
+              // Fetch the Pokémon details to get the sprites
+              fetch(pokemon.url)
+                .then(response => response.json())
+                .then(pokemonDetails => {
+                  const spriteURL = pokemonDetails.sprites.front_default;
+                  const image = document.createElement('img');
+                  image.src = spriteURL;
+                  listItem.prepend(image);
+                })
+                .catch(error => console.error('Error fetching Pokémon details:', error));
+  
+              listItem.addEventListener('click', () => fetchPokemonInfo(pokemon.url));
+              pokemonList.appendChild(listItem);
+            }
+          });
+        }
+  
+        // Initial display of Pokémon list
+        displayPokemonList();
+  
+        // Event listener for the search box
+        searchBox.addEventListener('input', () => {
+          const searchQuery = searchBox.value.trim();
+          displayPokemonList(searchQuery);
         });
       })
       .catch(error => console.error('Error fetching Pokémon data:', error));
@@ -45,4 +62,3 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error fetching Pokémon info:', error));
     }
   });
-  
