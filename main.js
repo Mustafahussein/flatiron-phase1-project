21b3aac262c4293e10f07efcadf040870fc4c115
Mustafa Hussein <mustafahussein.a@gmail.com>
2023-08-01 00:47:10 -0400

    chanaged my project API

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-console.log('main.js connected')
+console.log('main.js connected');
 
-const handleFormInputFocus = () => {
-    console.log(`focus occured`)
+const searchTermsInput = document.body.querySelector("#search-terms");
+
+const getMealCategories = async () => {
+    const getMealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    //If I don't want to use the awaith method like the video, I can use the .then lie below:
+    //const response = await fetch(getMealCategoriesApiURL).then(response => response.json())
+    const response = await fetch(getMealCategoriesApiURL)
+
+    const data = response.json()
+
+    console.log(data)
 }
 
-const searchTermsInput = document.body.querySelector("#search-terms")
-searchTermsInput.addEventListener('focus', handleFormInputFocus)
+const handleFormInputFocus = async () => {
+    console.log(`focus occured`);
+
+    await getMealCategories();
+}
+
+searchTermsInput.addEventListener('focus', handleFormInputFocus);
